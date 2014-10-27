6ac7c253d4be1c950be39578fdb546b795dc0eb6
Lyza Danger Gardner <lyza@lyza.com>
2014-10-27 15:25:17 -0700

    Compiling basic handlebars

--- build/gulp/patternTemplates.js
+++ build/gulp/patternTemplates.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var gulp   = require('gulp');
-var rename = require('gulp-rename');
+var gulp       = require('gulp');
+var template   = require('gulp-compile-handlebars');
+var rename     = require('gulp-rename');
+
+var templateData = {'foo' : 'bar'};
 
 module.exports = function() {
+
   return gulp.src('./source/patterns/**/*.hbs')
+    .pipe(template(templateData))
     .pipe(rename({extname: '.html'}))
     .pipe(gulp.dest('./static'));
 };
