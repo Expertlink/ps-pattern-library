dda760d6d63d3fb38bf597a167a5d237d9883267
Lyza Danger Gardner <lyza@lyza.com>
2014-10-28 11:22:15 -0700

    Extend, don't trounce, file.data

--- build/gulp/templateData.js
+++ build/gulp/templateData.js
@@ -15,7 +15,7 @@ module.exports = function (options) {
 	}, options || {});
 
 	return through.obj(function (file, enc, cb) {
-		var relPath, absPath;
+		var relPath, absPath, existingData;
 
 		if (file.isNull()) {
 			this.push(file);
@@ -31,7 +31,8 @@ module.exports = function (options) {
 		absPath = path.resolve(path.dirname(file.path), relPath);
 
 		try {
-			file[options.property] = JSON.parse(fs.readFileSync(absPath));
+      existingData = file[options.property] || {};
+			file[options.property] = _.extend(existingData, JSON.parse(fs.readFileSync(absPath)));
 		} catch (err) {
 			// this.emit('error', new util.PluginError(PLUGIN_NAME, err));
 		}
