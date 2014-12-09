a26cb52d2ef2c8d210ea8c65cd44011f7569ae4d
Tyler Sticka <tyler@tylersticka.com>
2014-12-09 15:16:45 -0800

    fraction and currency helpers

--- source/patterns/templateHelpers.js
+++ source/patterns/templateHelpers.js
@@ -19,8 +19,43 @@ module.exports = {
       return block.inverse(this);
     }
   },
-  random: function random() {
+  random: function random () {
     var args = Array.prototype.slice.call(arguments, 0, -1);
     return args[Math.floor(Math.random() * args.length)];
+  },
+  formatCurrency: function formatCurrency (value) {
+    return value.toFixed(2);
+  },
+  formatFraction: function formatFraction (value) {
+    var fractionCodes = {
+      25: 188,
+      50: 189,
+      75: 190,
+      33: 8531,
+      66: 8532,
+      20: 8533,
+      40: 8534,
+      60: 8535,
+      80: 8536,
+      16: 8537,
+      83: 8538,
+      12: 8539,
+      37: 8540,
+      62: 8541,
+      87: 8542
+    };
+    if (parseInt(value) !== value) {
+      var integer = Math.floor(value);
+      var decimal = value - integer;
+      var key = Math.floor(decimal * 100);
+      if (fractionCodes.hasOwnProperty(key)) {
+        value = integer + '&#' + fractionCodes[key] + ';';
+      }
+    }
+    return value;
+  },
+  replace: function replace (str, a, b) {
+    str = str + '';
+    return str.split(a).join(b);
   }
 };
