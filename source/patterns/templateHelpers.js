6f8c664121e5f96ef31f57caaaa8eeb39d5c4045
Tyler Sticka <tyler@tylersticka.com>
2014-12-09 14:50:02 -0800

    tweak to {{times}} to allow access to {{@index}}

--- source/patterns/templateHelpers.js
+++ source/patterns/templateHelpers.js
@@ -1,11 +1,14 @@
 'use strict';
+var Handlebars  = require('handlebars');
 /* These helpers are available to all Handlebars templates
  * compiled by the styleGuide task */
 module.exports = {
   times: function times (n, block) {
-    var accum = '';
+    var accum = '', data;
     for (var i = 0; i < n; ++i) {
-      accum += block.fn(i);
+      data = Handlebars.createFrame(block.data || {});
+      data.index = i;
+      accum += block.fn(i, { data: data });
     }
     return accum;
   },
