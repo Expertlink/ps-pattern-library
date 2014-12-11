59b8dfb2921eba608a92e9d5e54a8e3e42ed77e4
Tyler Sticka <tyler@tylersticka.com>
2014-12-11 11:06:14 -0800

    formatSlug template helper

--- source/patterns/templateHelpers.js
+++ source/patterns/templateHelpers.js
@@ -57,5 +57,15 @@ module.exports = {
   replace: function replace (str, a, b) {
     str = str + '';
     return str.split(a).join(b);
+  },
+  formatSlug: function formatSlug (str) {
+    return str
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, '-')       // Replace spaces with -
+      .replace(/[^\w\-]+/g, '-')  // Remove all non-word chars
+      .replace(/\-\-+/g, '-')     // Replace multiple - with single -
+      .replace(/^-+/, '')         // Trim - from start of text
+      .replace(/-+$/, '');        // Trim - from end of text
   }
 };
