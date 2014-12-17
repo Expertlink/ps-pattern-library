5ddedafe022c07780cf53b88a08f908b6aa7de32
Lyza Danger Gardner <lyza@lyza.com>
2014-12-17 11:57:52 -0800

    Separating out pattern library styles/scss from site styles/scss. Updating affected components

--- pattern-lib/tasks/library-styles.js
+++ pattern-lib/tasks/library-styles.js
@@ -1,14 +1,17 @@
 /* global console */
 'use strict';
 var gulp         = require('gulp'),
+    gulpif       = require('gulp-if'),
+    concat       = require('gulp-concat'),
     autoprefixer = require('gulp-autoprefixer'),
     settings     = require('../../settings'),
     sass         = require('gulp-ruby-sass');
 
 module.exports = function () {
   return gulp.src(settings.src.library.styles)
-    .pipe(sass())
+    .pipe(gulpif(/[.]scss/, sass()))
     .on('error', function(err) { console.log(err.message); })
     .pipe(autoprefixer())
+    .pipe(concat('pattern-library.css'))
     .pipe(gulp.dest(settings.dest.library.css));
 };
