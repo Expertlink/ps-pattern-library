7ab23727fbccdbed0b1d9688e17e73ded1ce7e8b
Lyza Danger Gardner <lyza@lyza.com>
2015-01-16 16:00:46 -0800

    Moving site-/project-specific gulp tasks into a new location in source/build; new gulp task modules

--- gulpfile.js
+++ gulpfile.js
@@ -27,30 +27,10 @@ taskModules.forEach(function(task) {
 require(settings.paths.library + '/tasks/');
 
 /**
- * More site-building tasks, not modules.
+ * Include site and project-specific tasks.
  */
-gulp.task('assets', function() {
- return gulp.src(settings.src.assets)
- .pipe(gulp.dest(settings.dest.assets));
-});
-gulp.task('dist', function() {
- return gulp.src(settings.src.static)
- .pipe(gulp.dest(settings.paths.dist));
-});
+require(settings.paths.gulpSiteTasks);
 
-var svgInlineConfig = {
-  mode: {
-    symbol: {}
-  }
-};
-/**
- * Site-building specific to this project
- */
- gulp.task('sprites-inline', function(){
-   gulp.src(settings.src.site.sprites.inline)
-   .pipe(svgSprite(svgInlineConfig))
-   .pipe(gulp.dest(settings.dest.site.sprites));
- });
 
 gulp.task('site', ['styles', 'scripts', 'assets']);
 gulp.task('default', ['site', 'library', 'watch', 'browserSync']);
---
+++ source/build/tasks/index.js
@@ -0,0 +1,18 @@
+/* global console */
+/**
+* gulpfile for site-/project-specific tasks
+*/
+'use strict';
+
+var gulp     = require('gulp');
+var settings = require('../../../settings');
+
+gulp.task('svg-sprites-inline', require('./svg-sprites-inline'));
+gulp.task('assets', function() {
+  return gulp.src(settings.src.assets)
+  .pipe(gulp.dest(settings.dest.assets));
+});
+gulp.task('dist', function() {
+  return gulp.src(settings.src.static)
+  .pipe(gulp.dest(settings.paths.dist));
+});
