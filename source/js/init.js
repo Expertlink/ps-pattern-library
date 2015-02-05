adb60a3079392cbe9e10f3100e5037d19b267bbc
Lyza Danger Gardner <lyza@lyza.com>
2015-02-05 09:23:10 -0800

    Adding jQuery Succinct plugin; adding data-truncate attrs to trunacted review elements

--- source/js/init.js
+++ source/js/init.js
@@ -1,3 +1,14 @@
 $(function() {
+  'use strict';
   FastClick.attach(document.body);
+
+  // Truncate (succinct) plugin
+  // Elements with `[data-truncate]` attr
+  // Initialize to truncate them.
+  $('[data-truncate]').each(function() {
+    var length = $(this).data('truncate') || 240;
+    $(this).succinct({
+      size: length
+    });
+  });
 });
