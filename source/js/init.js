30ec483282fb4da9c048262a9cc0a4de0bc8faa3
Tyler Sticka <tyler@tylersticka.com>
2015-02-26 16:15:15 -0800

    organizing and commenting solution to #236

--- source/js/init.js
+++ source/js/init.js
@@ -15,23 +15,35 @@ $(function() {
     }
   });
 
-  $(document).on('touchstart.vse.no-zoom mousedown.vse.no-zoom', '.form-control', function () {
-    var $element = $(this);
-    clearTimeout($element.data('vse.no-zoom.removal-timeout'));
-    clearTimeout($element.data('vse.no-zoom.focus-timeout'));
-    $element.addClass('no-zoom');
-    $element.one('touchend.vse.no-zoom mouseup.vse.no-zoom', function () {
-      $element.data('vse.no-zoom.removal-timeout', setTimeout(function () {
-        $element.removeClass('no-zoom');
-      }, 300));
+  // Counteract input zoom on some touch devices
+  if (Modernizr.touch) {
+    // Must trigger on touchstart or emulated mousedown, as either could fire
+    // (and touchend/mouseup events can fire too late)
+    $('.form-control').on('touchstart.vse.no-zoom mousedown.vse.no-zoom', function () {
+      // Store this element
+      var $element = $(this);
+      // Clear preexisting timeouts
+      clearTimeout($element.data('vse.no-zoom.removal-timeout'));
+      clearTimeout($element.data('vse.no-zoom.focus-timeout'));
+      // Add the no-zoom helper class
+      $element.addClass('no-zoom');
+      // On touchend or mouseup, remove the class after a brief delay
+      $element.one('touchend.vse.no-zoom mouseup.vse.no-zoom', function () {
+        $element.data('vse.no-zoom.removal-timeout', setTimeout(function () {
+          $element.removeClass('no-zoom');
+        }, 300)); // Increase this delay if ineffective
+      });
+      // On focus, attach an additional class that will prevent the font-size
+      // from changing (for example, on text selection or movement of the caret)
+      $element.one('focus.vse.no-zoom', function () {
+        $element.data('vse.no-zoom.focus-timeout', setTimeout(function () {
+          $element.addClass('no-zoom-focused');
+          $element.one('blur.vse.no-zoom', function () {
+            $element.removeClass('no-zoom-focused');
+          });
+        }, 500)); // Increase this delay if this conflicts with touchend event
+      });
     });
-    $element.one('focus.vse.no-zoom', function () {
-      $element.data('vse.no-zoom.focus-timeout', setTimeout(function () {
-        $element.addClass('no-zoom-focused');
-      }, 500));
-    });
-    $element.one('blur.vse.no-zoom', function () {
-      $element.removeClass('no-zoom-focused');
-    });
-  });
+  }
+
 });
