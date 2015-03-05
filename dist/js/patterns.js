b4b49dd6eab8752548181e528a5979c46768d814
Tyler Sticka <tyler@tylersticka.com>
2015-03-05 12:10:26 -0800

    dist 📦

--- dist/js/patterns.js
+++ dist/js/patterns.js
@@ -193,92 +193,6 @@ $(window).load(function(){
     }
   });
 
-// Scripts for pattern at ../source/patterns/02-components/forms/06-phone-country.hbs
-
-$(function() {
-  var countryData     = $.fn.intlTelInput.getCountryData(),
-  countryCount    = countryData.length,
-  countryOptions  = [];
-
-  for (i = 0; i < countryCount; i++) { // Build <option>s for country select(s)
-    countryOptions.push($('<option></option>')
-    .attr('value', countryData[i].iso2)
-    .text(countryData[i].name)
-  );
-  }
-
-  $('[data-mask-type="tel"]').each(function() { // Set up phone masking
-    var $el         = $(this),
-    countrySelector = $el.data('country-select'),
-    $country        = $(countrySelector);
-
-    $country.append(countryOptions).val('us');  // Add <option>s and set to 'us' by default
-    $country.change(function() {
-      $el.intlTelInput('selectCountry', $(this).val());
-    });
-  });
-});
-
-// Scripts for pattern at ../source/patterns/02-components/forms/_promo-code.hbs
-
-
-  $(function() {
-
-    $('.promo-group').each(function() {
-      var $promoGroup = $(this);
-      var $applyBtn   = $promoGroup.find('.apply-btn');
-      var $input      = $promoGroup.find('input');
-
-      $applyBtn.click(function(e) {
-        var code          = $input.val();
-        var codeIsValid   = !!code.match(/^\d{6}/);
-        var $els          = {
-          discounts     : $('[data-discount-for="' + $promoGroup.attr('id') + '"]'),
-          promoGroup    : $promoGroup,
-          success       : $('[data-promo-success-for="' + $promoGroup.attr('id') + '"]'),
-          toggleControl : $('[href="#' + $promoGroup.attr('id') + '"]'),
-          input         : $input
-        };
-        var eventName  = (codeIsValid) ? 'c4.codeIsValid' : 'c4.codeIsInvalid';
-        $promoGroup.trigger('c4.codeValidation', [code, codeIsValid, $els]);
-        $promoGroup.trigger(eventName, [code, $els]);
-        e.preventDefault();
-      });
-
-      $('.promo-group').on('c4.codeValidation', function(e, code, codeIsValid, $els) {
-        $els.promoGroup.toggleClass('has-error', !codeIsValid);
-        $els.promoGroup.toggleClass('has-success', codeIsValid);
-        $els.success.toggleClass('has-success', codeIsValid);
-      });
-
-      $('.promo-group').on('c4.codeIsValid', function(e, code, $els) {
-        $els.success.find('.js-code-slot').text(code);
-        $promoGroup.collapse('hide');
-        $els.toggleControl.show().html('Change code');
-        $els.discounts.find('.js-price-amount').each(function() {
-          $(this).html('<del>$' + $(this).data('amount') + '</del>');
-        });
-        $els.discounts.find('.js-price-discounted').each(function() {
-          $(this).html('$' + $(this).data('amount'));
-        });
-      });
-
-      $('.promo-group').on('c4.codeIsInvalid', function(e, code, $els) {
-        $els.toggleControl.html('Cancel');
-        $promoGroup.one('hidden.bs.collapse', function() {
-          $els.toggleControl.html('Have a promo code?');
-        });
-        $els.discounts.find('.js-price-amount').each(function() {
-          $(this).html('$' + $(this).data('amount'));
-        });
-        $els.discounts.find('.js-price-discounted').each(function() {
-          $(this).html('');
-        });
-      });
-    });
-  });
-
-
 // Scripts for pattern at ../source/patterns/02-components/main-nav/01-main-nav.hbs
 
 +function ($) {
@@ -418,27 +332,91 @@ $(function(){
 
 });
 
-// Scripts for pattern at ../source/patterns/pages/account/02-add.hbs
+// Scripts for pattern at ../source/patterns/02-components/forms/06-phone-country.hbs
 
-$(function () {
-  var $paymentOptions = $('.js-payment-options');
+$(function() {
+  var countryData     = $.fn.intlTelInput.getCountryData(),
+  countryCount    = countryData.length,
+  countryOptions  = [];
 
-  $paymentOptions.on('change', '.js-payment-option', function (event) {
-    var $trigger = $(event.currentTarget);
-    var triggerData = $trigger.data();
-    var hasCollapseTarget = triggerData.target !== undefined;
+  for (i = 0; i < countryCount; i++) { // Build <option>s for country select(s)
+    countryOptions.push($('<option></option>')
+    .attr('value', countryData[i].iso2)
+    .text(countryData[i].name)
+  );
+  }
 
-    if (hasCollapseTarget) {
-      $(triggerData.target)
-        .collapse('show')
-        .addClass('js-expanded')
-    } else {
-      $paymentOptions.find('.js-expanded')
-        .collapse('hide')
-        .removeClass('js-expanded');
-    }
+  $('[data-mask-type="tel"]').each(function() { // Set up phone masking
+    var $el         = $(this),
+    countrySelector = $el.data('country-select'),
+    $country        = $(countrySelector);
+
+    $country.append(countryOptions).val('us');  // Add <option>s and set to 'us' by default
+    $country.change(function() {
+      $el.intlTelInput('selectCountry', $(this).val());
+    });
   });
 });
+
+// Scripts for pattern at ../source/patterns/02-components/forms/_promo-code.hbs
+
+
+  $(function() {
+
+    $('.promo-group').each(function() {
+      var $promoGroup = $(this);
+      var $applyBtn   = $promoGroup.find('.apply-btn');
+      var $input      = $promoGroup.find('input');
+
+      $applyBtn.click(function(e) {
+        var code          = $input.val();
+        var codeIsValid   = !!code.match(/^\d{6}/);
+        var $els          = {
+          discounts     : $('[data-discount-for="' + $promoGroup.attr('id') + '"]'),
+          promoGroup    : $promoGroup,
+          success       : $('[data-promo-success-for="' + $promoGroup.attr('id') + '"]'),
+          toggleControl : $('[href="#' + $promoGroup.attr('id') + '"]'),
+          input         : $input
+        };
+        var eventName  = (codeIsValid) ? 'c4.codeIsValid' : 'c4.codeIsInvalid';
+        $promoGroup.trigger('c4.codeValidation', [code, codeIsValid, $els]);
+        $promoGroup.trigger(eventName, [code, $els]);
+        e.preventDefault();
+      });
+
+      $('.promo-group').on('c4.codeValidation', function(e, code, codeIsValid, $els) {
+        $els.promoGroup.toggleClass('has-error', !codeIsValid);
+        $els.promoGroup.toggleClass('has-success', codeIsValid);
+        $els.success.toggleClass('has-success', codeIsValid);
+      });
+
+      $('.promo-group').on('c4.codeIsValid', function(e, code, $els) {
+        $els.success.find('.js-code-slot').text(code);
+        $promoGroup.collapse('hide');
+        $els.toggleControl.show().html('Change code');
+        $els.discounts.find('.js-price-amount').each(function() {
+          $(this).html('<del>$' + $(this).data('amount') + '</del>');
+        });
+        $els.discounts.find('.js-price-discounted').each(function() {
+          $(this).html('$' + $(this).data('amount'));
+        });
+      });
+
+      $('.promo-group').on('c4.codeIsInvalid', function(e, code, $els) {
+        $els.toggleControl.html('Cancel');
+        $promoGroup.one('hidden.bs.collapse', function() {
+          $els.toggleControl.html('Have a promo code?');
+        });
+        $els.discounts.find('.js-price-amount').each(function() {
+          $(this).html('$' + $(this).data('amount'));
+        });
+        $els.discounts.find('.js-price-discounted').each(function() {
+          $(this).html('');
+        });
+      });
+    });
+  });
+
 
 // Scripts for pattern at ../source/patterns/pages/advisor-bio/01-advisor-bio.hbs
 
@@ -488,3 +466,25 @@ $(function(){
 
 });
 
+
+// Scripts for pattern at ../source/patterns/pages/account/02-add.hbs
+
+$(function () {
+  var $paymentOptions = $('.js-payment-options');
+
+  $paymentOptions.on('change', '.js-payment-option', function (event) {
+    var $trigger = $(event.currentTarget);
+    var triggerData = $trigger.data();
+    var hasCollapseTarget = triggerData.target !== undefined;
+
+    if (hasCollapseTarget) {
+      $(triggerData.target)
+        .collapse('show')
+        .addClass('js-expanded')
+    } else {
+      $paymentOptions.find('.js-expanded')
+        .collapse('hide')
+        .removeClass('js-expanded');
+    }
+  });
+});
